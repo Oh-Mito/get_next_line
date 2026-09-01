19e2add29a4abd59167a7d55153ebd39bed0aefc
Ohmura Mito <omito@c2r3s7.42tokyo.jp>
2026-09-01 09:24:03 +0900

    3

--- test_read.c
+++ test_read.c
@@ -1,16 +1,40 @@
 #include <unistd.h>
-#include <sys/types.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 #include <stdio.h>
 
-# define BUF_SIZE 9
+# define BUF_SIZE 1
 
+//int main(void)
+//{
+//	int	fd;
+//	int	byte_num;
+//	char	buf[BUF_SIZE + 1];
+//
+//	fd = open("test.txt", O_RDONLY);
+//	if (fd == -1)
+//	{
+//		write(1, "error", 5);
+//		return (1);
+//	}
+//	byte_num = read(fd, &buf, BUF_SIZE);
+//	if (byte_num == -1)
+//	{
+//		write (1, "read_error", 10);
+//		return (1);
+//	}
+//	printf("test.txtから%dバイト読み込みました！\n", byte_num);
+//	buf[byte_num] = '\0';
+//	printf("読み込んだ文字列%s\n", buf);
+//
+//	close(fd);
+//	return(0);
+//}
+//
 int main(void)
 {
-	int	fd;
-	int	byte_num;
-	char	buf[BUF_SIZE];
+	int fd;
+	int byte_num;
+	char buf[BUF_SIZE + 1];
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
@@ -18,16 +42,14 @@ int main(void)
 		write(1, "error", 5);
 		return (1);
 	}
-	byte_num = read(fd, &buf, BUF_SIZE);
-	if (byte_num == -1)
+	while
 	{
-		write (1, "read_error", 10);
-		return (1);
+		byte_num += read(fd, &buf, 1);
+		if (byte_num == 0)
+			break ;
+		write(1, &buf[byte_num], 1);
 	}
-	printf("test.txtから%dバイト書き込みました！\n", byte_num);
-	buf[byte_num] = '\0';
-	printf("書き込んだ文字列%s\n", buf);
 
-	close(fd);
-	return(0);
+
+
 }
